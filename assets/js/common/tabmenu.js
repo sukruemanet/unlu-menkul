f4ce7484a34bd77dff6ea61ee10ee36dc10baad9
Adın Soyadın <email@adresin.com>
2026-06-29 16:36:02 +0300

    test

--- assets/js/common/tabmenu.js
+++ assets/js/common/tabmenu.js
@@ -2,17 +2,21 @@ $(document).ready(function () {
   $("ul.tabs li").click(function () {
     var tab_id = $(this).attr("data-tab");
 
-    $("ul.tabs li").removeClass("current");
-    $(".tab-content").removeClass("current");
-
+    // Sadece bu li'nin bulunduğu ul'deki kardeş li'leri etkile
+    var $parentUl = $(this).closest("ul.tabs");
+    $parentUl.find("li").removeClass("current");
     $(this).addClass("current");
-    $("#" + tab_id).addClass("current");
 
-    // Soft fade-up animasyonu
+    // Sadece bu ul'ün içinde bulunduğu tab-menu-content'teki tab-content'leri etkile
+    var $tabContainer = $parentUl.closest(".tab-menu-content");
+    $tabContainer.find(".tab-content").removeClass("current");
+    $tabContainer.find("#" + tab_id).addClass("current");
+
+    // Animasyon
     gsap.fromTo(
       "#" + tab_id,
       { opacity: 0, y: 30 },
-      { opacity: 1, y: 0, duration: 0.6, ease: "power2.out" }
+      { opacity: 1, y: 0, duration: 0.6, ease: "power2.out" },
     );
   });
 });
